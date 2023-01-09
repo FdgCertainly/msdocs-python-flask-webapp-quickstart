74638e8926cce43206b37a502044191c2e25c3e1
FdgCertainly <94852583+FdgCertainly@users.noreply.github.com>
2023-01-09 18:29:24 +0100

    first commit

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+
+from hello import answer_query_with_context 
+from hello  import df
+from hello  import document_embeddings
+from hello import order_document_sections_by_query_similarity
+from hello import vector_similarity
+from hello import load_embeddings
+from hello import get_query_embedding
+from hello import construct_prompt
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/chat')
 def index():
    print('Request for index page received')
-   return render_template('index.html')
+   return answer_query_with_context("Do you have 90s dresses?", df, document_embeddings)
+#    return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
@@ -23,6 +34,7 @@ def hello():
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
+
 
 
 if __name__ == '__main__':
